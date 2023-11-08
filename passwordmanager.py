fc3a279418dbb4026d0d784ceba0348316c26ac4
wubro0e <mohammedmustafa5205@gmail.com>
2023-11-08 16:32:07 -0600

    "Implemented search and view password functions"

--- passwordmanager.py
+++ passwordmanager.py
@@ -2,9 +2,15 @@
 
 # Input: add new password- gen new password; view all passwords
 
+import os
+
+
 def view_pass():
-    with open('passwords.txt', 'a') as file:
-        
+    if os.path.exists('passwords.txt'):
+        with open('passwords.txt', 'r') as file:
+            for line in file:
+                username, password = line.split(" ")
+                print("username: ", username, " password: ", password, "\n")
     return
 
 
@@ -13,13 +19,21 @@ def add_pass():
     newpass = input("Enter new password:\n")
 
     with open('passwords.txt', 'a') as file:
-        file.write(username+" "+newpass)
+        file.write(username+" "+newpass + "\n")
 
     return
 
 
 def create_pass():
     return
+
+
+def search(findusername):
+    with open('passwords.txt', 'r') as file:
+        for line in file:
+            username, password = line.split(" ")
+            if username == findusername:
+                print(password)
 
 
 main_pass = "123a"
@@ -30,13 +44,18 @@ else:
     flag = False
 
 while flag == True:  # view passwords #add password #create password
+
     case = input(
-        "View all passwords, Add a new password or Create a new password (view,add,create)\n")
+        "View all passwords, Add a new password, Create a new password or Search for a password(view,add,create,search)\n")
     if case == "view":
         view_pass()
     elif case == "add":
         add_pass()
     elif case == "create":
         create_pass
+    elif case == "search":
+        username = input("Enter a username to view its password\n")
+        search(username)
+
     else:
         print("Invalid Input, Try Again.\n")
