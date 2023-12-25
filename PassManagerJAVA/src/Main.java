9b8e2db75fc2f65dbb8ab36de9d11f2373401262
wubro0e <mohammedmustafa5205@gmail.com>
2023-12-25 14:54:33 -0600

    "Java layout"

--- PassManagerJAVA/src/Main.java
+++ PassManagerJAVA/src/Main.java
@@ -1,17 +1,70 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Enter main Password");
+        String main_pass = "a";
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in);
+        String temp = in.next();
+
+        Boolean flag = false;
+        if ( temp.equals(main_pass))
+        {
+            flag = true;
         }
-    }
+        else{
+            flag = false;
+        }
+
+        while (flag == true)
+        {
+            System.out.println("View all passwords, Add a new password, Create a new password or Search for a password (view,add,create,search and q to quit)");
+            String option = in.next();
+            switch (option)
+            {
+                case "view": 
+                {
+                    view_pass();
+                    break;
+                }
+                case "add":
+                {
+                    System.out.println("Enter new Username:");
+                    String username = in.next(); 
+                    System.out.println("Enter new password");
+                    String newpass = in.next();
+                    add_pass(username, newpass);
+                    break;
+                    
+                }
+                case "create":
+                {
+                    System.out.println("How long should the password be?");
+                    int length = in.nextInt();
+                    create_pass(length);
+                    break;
+                }
+
+                case "search":
+                {
+                    System.out.println("Enter a username to view its password");
+                    String username = in.next();
+                    search(username);
+                    break;
+        
+                }
+                case "q":
+                {
+                    flag = false;
+                    break;
+                }
+                default: System.out.println("Invalid input, Try again.");
+            }
+        }
+
+        }
+
 }
+    
